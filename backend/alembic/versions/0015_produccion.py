a85501c7ad1785cd154bd0e30c2d1fe1911e1d6e
tpros68 <tpros687@gmail.com>
2026-05-06 20:05:25 -0300

    fix(migration): restore 0015_produccion revision ID to match Railway DB state

--- backend/alembic/versions/0015_produccion.py
+++ backend/alembic/versions/0015_produccion.py
@@ -1,6 +1,6 @@
 """add lotes_ganado, eventos_reproductivos, ciclos_agricolas
 
-Revision ID: 0015_prod
+Revision ID: 0015_produccion
 Revises: 0014_coneat
 Create Date: 2026-05-06
 """
@@ -9,7 +9,7 @@ from typing import Union
 import sqlalchemy as sa
 from alembic import op
 
-revision: str = "0015_prod"
+revision: str = "0015_produccion"
 down_revision: Union[str, None] = "0014_coneat"
 branch_labels = None
 depends_on = None
